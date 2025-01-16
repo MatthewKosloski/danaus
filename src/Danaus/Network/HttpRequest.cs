723774fd612634e655353a9b7f0f1d285753a37f
Matthew Kosloski <matthewkosloski@me.com>
2025-01-16 05:32:41 -0600

    feat(HttpRequest): create functions to build requests from URLs

--- src/Danaus/Network/HttpRequest.cs
+++ src/Danaus/Network/HttpRequest.cs
@@ -5,6 +5,8 @@ namespace Danaus.Network;
 
 public class HttpRequest(URL url, HttpMethod method, Dictionary<HttpRequestHeader, string> headers)
 {
+    public const string USER_AGENT = "Danaus/0.0.0"; 
+
     public URL Url { get; set; } = url;
     public HttpMethod Method { get; set; } = method;
     public Dictionary<HttpRequestHeader, string> Headers { get; set; } = headers;
@@ -26,5 +28,31 @@ public class HttpRequest(URL url, HttpMethod method, Dictionary<HttpRequestHeade
         builder.Append("\r\n");
 
         return builder.ToString();
+    }
+
+    public static HttpRequest FromURLString(string url, HttpMethod? method = null, Dictionary<HttpRequestHeader, string>? headers = null)
+    {
+        return FromURL(URLParser.Parse(url).Url, method, headers);
+    }
+
+    public static HttpRequest FromURL(URL url, HttpMethod? method = null, Dictionary<HttpRequestHeader, string>? headers = null)
+    {
+        if (url.Host is null)
+        {
+            throw new ArgumentException("Url must have a non-null host.");
+        }
+
+        var userAgentHeaders = new Dictionary<HttpRequestHeader, string>()
+        {
+            { HttpRequestHeader.Host, url.Host },
+            { HttpRequestHeader.UserAgent, USER_AGENT },
+        };
+
+        var combinedHeaders = headers is not null
+            ? headers.Concat(userAgentHeaders)
+                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value)
+            : userAgentHeaders;
+
+        return new HttpRequest(url, method ?? HttpMethod.Get, combinedHeaders);
     }
 }
