acf4c01b62fbb772091e7a20df595edab821e86a
Matthew Kosloski <matthewkosloski@me.com>
2025-02-01 01:38:35 -0600

    feat(HTMLTokenizer): implemented more states

--- src/Danaus/HTML/HTMLTokenizer.cs
+++ src/Danaus/HTML/HTMLTokenizer.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Danaus.Core;
 
 namespace Danaus.HTML;
@@ -105,6 +106,8 @@ class HTMLTokenizer(StreamReader input)
 
     private bool ShouldReconsume = false;
 
+    private StringBuilder TempBuffer = new();
+
     public HTMLToken? NextToken()
     {
         while (true)
@@ -116,6 +119,7 @@ class HTMLTokenizer(StreamReader input)
 
             switch (State)
             {
+                // https://html.spec.whatwg.org/multipage/parsing.html#data-state
                 case State.Data:
                 {
                     if (CurrentCharacter.Is(CodePoint.Ampersand))
@@ -143,6 +147,100 @@ class HTMLTokenizer(StreamReader input)
 
                     break;
                 }
+                // https://html.spec.whatwg.org/multipage/parsing.html#rcdata-state
+                case State.RCDATA:
+                {
+                    if (CurrentCharacter.Is(CodePoint.Ampersand))
+                    {
+                        ReturnState = State.RCDATA;
+                        SwitchTo(State.CharacterReference);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.LessThanSign))
+                    {
+                        SwitchTo(State.RCDATALessThanSign);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.NullCharacter))
+                    {
+                        // This is an unexpected-null-character parse error.
+                        EmitReplacementCharacterToken();
+                    }
+                    else if (IsEOF())
+                    {
+                        EmitEndOfFileToken();
+                    }
+                    else
+                    {
+                        EmitCurrentCharacterAsCharacterToken();
+                    }
+
+                    break;
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#rawtext-state
+                case State.RAWTEXT:
+                {
+                    if (CurrentCharacter.Is(CodePoint.LessThanSign))
+                    {
+                        SwitchTo(State.RAWTEXTLessThanSign);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.NullCharacter))
+                    {
+                        // This is an unexpected-null-character parse error.
+                        EmitReplacementCharacterToken();
+                    }
+                    else if (IsEOF())
+                    {
+                        EmitEndOfFileToken();
+                    }
+                    else
+                    {
+                        EmitCurrentCharacterAsCharacterToken();
+                    }
+
+                    break; 
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#script-data-state
+                case State.ScriptData:
+                {
+                    if (CurrentCharacter.Is(CodePoint.LessThanSign))
+                    {
+                        SwitchTo(State.ScriptDataLessThanSign);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.NullCharacter))
+                    {
+                        // This is an unexpected-null-character parse error.
+                        EmitReplacementCharacterToken();
+                    }
+                    else if (IsEOF())
+                    {
+                        EmitEndOfFileToken();
+                    }
+                    else
+                    {
+                        EmitCurrentCharacterAsCharacterToken();
+                    }
+
+                    break; 
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#plaintext-state
+                case State.PLAINTEXT:
+                {
+                    if (CurrentCharacter.Is(CodePoint.NullCharacter))
+                    {
+                        // This is an unexpected-null-character parse error.
+                        EmitReplacementCharacterToken();
+                    }
+                    else if (IsEOF())
+                    {
+                        EmitEndOfFileToken();
+                    }
+                    else
+                    {
+                        EmitCurrentCharacterAsCharacterToken();
+                    }
+
+                    break; 
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#tag-open-state
                 case State.TagOpen:
                 {
                     if (CurrentCharacter.Is(CodePoint.ExclamationMark))
@@ -179,6 +277,109 @@ class HTMLTokenizer(StreamReader input)
 
                     break;
                 }
+                // https://html.spec.whatwg.org/multipage/parsing.html#end-tag-open-state
+                case State.EndTagOpen:
+                {
+                    if (CurrentCharacter.IsASCIIAlpha())
+                    {
+                        CreateNewEndTagToken(string.Empty);
+                        ReconsumeIn(State.TagName);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.GreaterThanSign))
+                    {
+                        // This is a missing-end-tag-name parse error.
+                        SwitchTo(State.Data);
+                    }
+                    else if (IsEOF())
+                    {
+                        // This is an eof-before-tag-name parse error.
+                        EmitCharacterToken(CodePoint.LessThanSign);
+                        EmitCharacterToken(CodePoint.Solidus);
+                        EmitEndOfFileToken();
+                    }
+                    else
+                    {
+                        // This is an invalid-first-character-of-tag-name parse error.
+                        CreateNewCommentToken(string.Empty);
+                        ReconsumeIn(State.BogusComment);
+                    }
+
+                    break;
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#tag-name-state
+                case State.TagName:
+                {
+                    if (IsWhiteSpace())
+                    {
+                        SwitchTo(State.BeforeAttributeName);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.Solidus))
+                    {
+                        SwitchTo(State.SelfClosingStartTag);
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.GreaterThanSign))
+                    {
+                        SwitchTo(State.Data);
+                        EmitCurrentToken();
+                    }
+                    else if (CurrentCharacter.IsASCIIUpperAlpha())
+                    {
+                        var token = GetCurrentTokenAsTagToken();
+                        char c = (char)CurrentCharacter;
+                        token.AppendToName(char.ToLower(c));
+                    }
+                    else if (CurrentCharacter.Is(CodePoint.NullCharacter))
+                    {
+                        // This is an unexpected-null-character parse error.
+                        var token = GetCurrentTokenAsTagToken();
+                        token.AppendToName((char)CodePoint.ReplacementCharacter);
+                    }
+                    else if (IsEOF())
+                    {
+                        // This is an eof-in-tag parse error.
+                        EmitEndOfFileToken();
+                    }
+                    else
+                    {
+                        var token = GetCurrentTokenAsTagToken();
+                        token.AppendToName((char)CurrentCharacter);
+                    }
+
+                    break;
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#rcdata-less-than-sign-state
+                case State.RCDATALessThanSign:
+                {
+                    if (CurrentCharacter.Is(CodePoint.Solidus))
+                    {
+                        TempBuffer = new(string.Empty);
+                        SwitchTo(State.RCDATAEndTagOpen);
+                    }
+                    else
+                    {
+                        EmitCharacterToken(CodePoint.LessThanSign);
+                        ReconsumeIn(State.RCDATA);
+                    }
+
+                    break;
+                }
+                // https://html.spec.whatwg.org/multipage/parsing.html#rcdata-end-tag-open-state
+                case State.RCDATAEndTagOpen:
+                {
+                    if (CurrentCharacter.IsASCIIAlpha())
+                    {
+                        CreateNewEndTagToken(string.Empty);
+                        ReconsumeIn(State.RCDATAEndTagName);
+                    }
+                    else
+                    {
+                        EmitCharacterToken(CodePoint.LessThanSign);
+                        EmitCharacterToken(CodePoint.Solidus);
+                        ReconsumeIn(State.RCDATA);
+                    }
+
+                    break;
+                }
             }
 
         }
@@ -208,6 +409,11 @@ class HTMLTokenizer(StreamReader input)
         CreateNewToken(new TagToken(TagTokenType.Start, name));
     }
 
+    private void CreateNewEndTagToken(string name)
+    {
+        CreateNewToken(new TagToken(TagTokenType.End, name));
+    }
+
     private void CreateNewCommentToken(string data)
     {
         CreateNewToken(new CommentToken(data));
@@ -216,6 +422,16 @@ class HTMLTokenizer(StreamReader input)
     private void EmitCurrentCharacterAsCharacterToken()
     {
         Tokens.Enqueue(new CharacterToken((char)CurrentCharacter));
+    }
+
+    private void EmitReplacementCharacterToken()
+    {
+        Tokens.Enqueue(new CharacterToken((char)CodePoint.ReplacementCharacter));
+    }
+
+    private void EmitCurrentToken()
+    {
+        Tokens.Enqueue(CurrentToken);
     }
     
     private void EmitCommentToken(string data)
@@ -233,9 +449,23 @@ class HTMLTokenizer(StreamReader input)
         Tokens.Enqueue(new EndOfFileToken());
     }
 
+    private TagToken GetCurrentTokenAsTagToken()
+    {
+        if (CurrentToken is not TagToken)
+        {
+            throw new InvalidOperationException("Current token is not a tag token.");
+        }
+        return (TagToken)CurrentToken;
+    }
+
     private bool IsEOF()
     {
         return CurrentCharacter == END_OF_FILE;
+    }
+
+    private bool IsWhiteSpace()
+    {
+        return CurrentCharacter.IsOneOf(CodePoint.Tab, CodePoint.LineFeed, CodePoint.FormFeed, CodePoint.Space);
     }
 
     private void ConsumeNextInputCharacter()
