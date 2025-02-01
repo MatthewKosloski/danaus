89250a849c92ddec9836cdf7e4ae18967767e7d9
Matthew Kosloski <matthewkosloski@me.com>
2025-02-01 01:34:25 -0600

    fix(TagToken): implement AppendToName method

--- src/Danaus/HTML/TagToken.cs
+++ src/Danaus/HTML/TagToken.cs
@@ -8,7 +8,12 @@ enum TagTokenType {
 class TagToken(TagTokenType type, string name, bool isSelfClosing = false): HTMLToken 
 {
     public TagTokenType Type { get; } = type;
-    public string Name { get; } = name;
+    public string Name { get; protected set; } = name;
     public bool IsSelfClosing { get; } = isSelfClosing;
     public Dictionary<string, string>? Attributes { get; } = [];
+
+    public void AppendToName(char c)
+    {
+        Name += c;
+    }
 }
