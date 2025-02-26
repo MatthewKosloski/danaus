319f0cbb83f72e8c9b83bf8e9f581bee94dc6118
Matthew Kosloski <matthewkosloski@me.com>
2025-02-26 01:37:03 -0600

    feat(TagToken): implement methods to append a character to an attribute name or value

--- src/Danaus/HTML/TagToken.cs
+++ src/Danaus/HTML/TagToken.cs
@@ -10,11 +10,42 @@ class TagToken(TagTokenType type, string name, bool isSelfClosing = false): HTML
     public TagTokenType Type { get; } = type;
     public string Name { get; protected set; } = name;
     public bool IsSelfClosing { get; } = isSelfClosing;
-    public Dictionary<string, string>? Attributes { get; } = [];
+    public Dictionary<string, string> Attributes { get; } = [];
 
     public void AppendToName(char c)
     {
         Name += c;
+    }
+
+    public void AppendToAttributeName(string name, char c)
+    {
+        bool hasAttribute = Attributes.TryGetValue(name, out string? value);
+
+        if (!hasAttribute)
+        {
+            throw new InvalidOperationException($"TagToken does not have an attribute ${name}");
+        }
+
+        if (value is not null)
+        {
+            Attributes.Remove(name);
+            Attributes.Add(name + c, value);
+        }
+    }
+
+    public void AppendToAttributeValue(string name, char c)
+    {
+        bool hasAttribute = Attributes.TryGetValue(name, out string? value);
+
+        if (!hasAttribute)
+        {
+            throw new InvalidOperationException($"TagToken does not have an attribute ${name}");
+        }
+
+        if (value is not null)
+        {
+            Attributes.Add(name, value + c);
+        }
     }
 
     public bool IsStart()
