98c43262e23accdbffbc0259d02743edca19cd95
Matthew Kosloski <matthewkosloski@me.com>
2025-04-16 19:40:18 -0500

    feat(HTML): add more tag names

--- src/Danaus/HTML/TagName.cs
+++ src/Danaus/HTML/TagName.cs
@@ -2,10 +2,25 @@ namespace Danaus.HTML;
 
 public sealed class TagName(string name): Core.TagName(name)
 {
+    public static TagName Base => new("base");
+    public static TagName Basefront => new("basefront");
+    public static TagName Bgsound => new("bgsound");
+    public static TagName Body => new("body");
+    public static TagName Br => new("br");
+    public static TagName Head => new("head");
+    public static TagName Html => new("html");
+    public static TagName Link => new("link");
+    public static TagName Meta => new("meta");
+    public static TagName Noframes => new("noframes");
+    public static TagName Noscript => new("noscript");
+    public static TagName Script => new("script");
+    public static TagName Style => new("style");
     public static TagName Svg => new("svg");
     public static TagName Table => new("table");
     public static TagName Tbody => new("tbody");
+    public static TagName Template => new("template");
     public static TagName Tfoot => new("tfoot");
     public static TagName Thead => new("thead");
+    public static TagName Title => new("title");
     public static TagName Tr => new("tr");
 }
