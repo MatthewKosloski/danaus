9f934a6e08dbd2843231d1d2a1800e27aa284d8c
Matthew Kosloski <matthewkosloski@me.com>
2025-09-18 06:29:40 -0500

    fix(DOM): implement equals and hashcode for DocumentType

--- src/Danaus/DOM/DocumentType.cs
+++ src/Danaus/DOM/DocumentType.cs
@@ -6,4 +6,29 @@ class DocumentType(string name, Document document): Node(document)
     public string Name { get; set; } = name;
     public string PublicID { get; set; } = string.Empty;
     public string SystemID { get; set; } = string.Empty;
+
+    // https://dom.spec.whatwg.org/#concept-node-equals
+    public override bool Equals(object? other)
+    {
+        if (other == null || other is not DocumentType)
+        {
+            return false;
+        }
+        else
+        {
+            var otherDocumentType = (DocumentType)other;
+
+            return Name == otherDocumentType.Name &&
+                   PublicID == otherDocumentType.PublicID &&
+                   SystemID == otherDocumentType.SystemID;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(
+            Name.GetHashCode(),
+            PublicID.GetHashCode(),
+            SystemID.GetHashCode());
+    }
 }
